b12d3389c240cb648d0475711b3a462874b94161
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-04-16 09:36:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,20 @@ def Eval_loss(teams):
     return loss
 
 def main():
-    st.title("Team Optimization App")
-    st.write("Enter the names and numbers of 21 players:")
+    st.title("Soccer Team Sorter App")
+    st.write("This app will randomly sort 21 players into 3 teams as equal as possible based on their number rankings.")
 
     playing = {}
     player_table = st.beta_container()
     with player_table:
-        st.write("Enter player names and numbers:")
+        st.write("Enter player names and numbers. make sure there are no repeating names:")
         playing = {}
         for i in range(1, 22):
             col1, col2 = st.beta_columns(2)
             with col1:
-                name = st.text_input()
+                name = st.text_input(f"Name {i}")
             with col2:
-                number = st.number_input(value=0)
+                number = st.number_input(f"Number {i}", value=0)
             if name:
                 playing[name] = number
 
