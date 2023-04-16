739a6608eeac9d3cee1b6ab3d4b12972e1190365
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-04-16 15:24:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pyperclip
+import re
 
 def Eval_loss(teams):
     vec_m = (np.mean(teams, axis=1) - (np.mean(teams)))
@@ -63,6 +64,10 @@ def main():
             st.write(f"**{IDs_str}**")
             st.write(f"mean={np.round(np.mean(teams[i]),2)} sd={np.round(np.std(teams[i]),2)}")
         st.write('------------------------------')
-
+        
+        if st.button("nice format"):
+            clean = re.sub(r'[0-9.]', '', str(IDs))
+            st.write(clean)
+            
 if __name__ == "__main__":
     main()
