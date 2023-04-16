16aa70ca3a8f5fa0a0b7a796c1bd4e364a9c9862
Benjamin Menashe <81152368+Benjamin-Menashe@users.noreply.github.com>
2023-04-16 08:59:05 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import numpy as np
+
+def Eval_loss(teams):
+    vec_m = (np.mean(teams, axis=1) - (np.mean(teams)))
+    loss_m = np.linalg.norm(vec_m)
+    vec_s = (np.std(teams, axis=1) - np.mean(np.std(teams, axis=1)))
+    loss_s = np.linalg.norm(vec_s)
+    loss = loss_m + loss_s
+    return loss
+
+def main():
+    st.title("Team Optimization App")
+    st.write("Enter the names and numbers of 21 players:")
+
+    playing = {}
+    for i in range(21):
+        name = st.text_input(f"Name {i+1}")
+        number = st.number_input(f"Number {i+1}")
+        playing[name] = number
+
+    if st.button("Optimize Teams"):
+        ps = np.random.permutation(21)
+        IDs = np.reshape(np.array(list(playing.keys()))[ps],(3,7))
+        teams = np.reshape(np.array(list(playing.values()))[ps],(3,7))
+        loss_pre = Eval_loss(teams)
+        jj = 0
+
+        while jj < 1000:   
+            tm_ind = np.random.choice(3, 2, replace=False)
+            plr_ind = np.random.choice(7, 2)
+            teams2 = np.copy(teams)
+            teams2[tm_ind[0]][plr_ind[0]],teams2[tm_ind[1]][plr_ind[1]] = teams[tm_ind[1]][plr_ind[1]],teams[tm_ind[0]][plr_ind[0]]
+            loss_post = Eval_loss(teams2)
+            if loss_post < loss_pre:
+                teams = teams2
+                loss_pre = loss_post
+                IDs[tm_ind[0]][plr_ind[0]],IDs[tm_ind[1]][plr_ind[1]] = IDs[tm_ind[1]][plr_ind[1]],IDs[tm_ind[0]][plr_ind[0]]
+            jj += 1
+
+        st.write("Optimized Teams:")
+        for i in range(3):
+            st.write(f"Team {i+1}:")
+            for j in range(7):
+                st.write(f"{IDs[i][j]} - {teams[i][j]}")
+
+if __name__ == "__main__":
+    main()
